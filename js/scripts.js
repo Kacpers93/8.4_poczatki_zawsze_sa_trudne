3fe1a723cf2c3dbacc643acbbbe251c4198be6ec
Kacper Wołoszyn <budzio161@gmail.com>
2017-12-06 23:58:22 +0100

    Add all needed files

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+//alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+//no duzo to tutaj tego nie było..
+
+/*
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dignissim feugiat egestas.
+Phasellus nec rhoncus urna. Sed nec varius enim. Integer et orci leo.
+
+Nunc malesuada vel ex a semper. 
+Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+Fusce ornare massa non luctus porttitor. Fusce iaculis odio id velit pellentesque, id auctor turpis molestie.
+*/
