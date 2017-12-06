7c2b9f25b39d126dd232157187994fae53f2afb2
Kacper Wołoszyn <budzio161@gmail.com>
2017-12-07 00:25:46 +0100

    Change content & Add html with new name

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,15 @@ var name = prompt('Enter your name');
 //alert('Hello, ' + name);
 console.log('Hello, ' + name);
 
-//no duzo to tutaj tego nie było..
+
+//Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 
 /*
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dignissim feugiat egestas.
-Phasellus nec rhoncus urna. Sed nec varius enim. Integer et orci leo.
-
-Nunc malesuada vel ex a semper. 
-Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-Fusce ornare massa non luctus porttitor. Fusce iaculis odio id velit pellentesque, id auctor turpis molestie.
+ In dignissim feugiat egestas. 
+ Phasellus nec rhoncus urna. Sed nec varius enim. 
+ 
+ Integer et orci leo. Nunc malesuada vel ex a semper. 
+ Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+ 
+ Fusce ornare massa non luctus porttitor. Fusce iaculis odio id velit pellentesque, id auctor turpis molestie.
 */
